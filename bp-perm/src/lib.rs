faabdac8813a6ffbf60d6b5dad24db63d672cdec
ercembu <ercembulut@gmail.com>
2023-01-09 16:10:45 +0100

    defined PermutationProof, problem with create, waiting on issue

---
+++ bp-perm/src/lib.rs
@@ -0,0 +1,39 @@
+#![allow(non_snake_case)]
+
+extern crate alloc;
+
+use alloc::borrow::Borrow;
+use alloc::vec::Vec;
+
+use core::iter;
+use curve25519_dalek::ristretto::{CompressedRistretto, RistrettoPoint};
+use curve25519_dalek::scalar::Scalar;
+use curve25519_dalek::traits::VartimeMultiscalarMul;
+use merlin::Transcript;
+
+use crate::errors::ProofError;
+use crate::transcript::TranscriptProtocol;
+
+#[derive(Clone, Debug)]
+pub struct PermutationProof {
+    pub(crate) L_vec: Vec<CompressedRistretto>,
+    pub(crate) R_vec: Vec<CompressedRistretto>,
+    pub(crate) a: Scalar,
+    pub(crate) b: Scalar,
+}
+
+impl PermutationProof {
+    /// Create Permutation Proof Based on Mr. Ke's arithmetic circuits
+
+    pub fn create(
+        transcript: &mut Transcript,
+        Q: &RistrettoPoint,
+        G_factors: &[Scalar],
+        H_factors: &[Scalar],
+        mut G_vec: Vec<RistrettoPoint>,
+        mut H_vec: Vec<RistrettoPoint>,
+        mut a_vec: Vec<Scalar>,
+        mut b_vec: Vec<Scalar>,
+    ) -> PermutationProof {
+    }
+}
