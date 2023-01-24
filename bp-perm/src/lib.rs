5ec1665db121ab7f72ca039f6b1ddcd22bfa5e0a
ercembu <ercembulut@gmail.com>
2023-01-24 14:00:51 +0100

    removed unnecessary files, changed definition to arithmetic circuits

--- bp-perm/src/lib.rs
+++ bp-perm/src/lib.rs
@@ -10,42 +10,40 @@ use curve25519_dalek_ng::ristretto::{CompressedRistretto, RistrettoPoint};
 use curve25519_dalek_ng::scalar::Scalar;
 use curve25519_dalek_ng::traits::VartimeMultiscalarMul;
 use merlin::Transcript;
+use rand::prelude::*;
+use bulletproofs::BulletproofGens;
+use bulletproofs::ProofError;
 
-mod errors;
-mod transcript;
-
-pub use crate::errors::ProofError;
-pub use crate::transcript::TranscriptProtocol;
 
 #[derive(Clone, Debug)]
-pub struct PermutationProof {
-    pub(crate) L_vec: Vec<CompressedRistretto>,
-    pub(crate) R_vec: Vec<CompressedRistretto>,
-    pub(crate) a: Scalar,
-    pub(crate) b: Scalar,
+pub struct ArithmeticCircuitProof {
+    L_vec: Vec<Scalar>
 }
 
-impl PermutationProof {
+impl ArithmeticCircuitProof {
     /// Create Permutation Proof Based on Mr. Ke's arithmetic circuits
 
     pub fn create(
-        transcript: &mut Transcript,
-        Q: &RistrettoPoint,
-        G_factors: &[Scalar],
-        H_factors: &[Scalar],
+        G: &RistrettoPoint,
+        H: &RistrettoPoint,
         mut G_vec: Vec<RistrettoPoint>,
         mut H_vec: Vec<RistrettoPoint>,
-        mut a_vec: Vec<Scalar>,
-        mut b_vec: Vec<Scalar>,
-    ) -> PermutationProof {
-        let mut L_vec = Vec::with_capacity(5);
-        let mut R_vec = Vec::with_capacity(5);
+        mut W_L: Vec<Vec<Scalar>>,
+        mut W_R: Vec<Vec<Scalar>>,
+        mut W_O: Vec<Vec<Scalar>>,
+        mut W_V: Vec<Vec<Scalar>>,
+        mut c: Vec<Scalar>,
+        mut a_L: Vec<Scalar>,
+        mut a_R: Vec<Scalar>,
+        mut a_O: Vec<Scalar>,
+        mut gamma: Vec<Scalar>
 
-        PermutationProof{
+    ) -> ArithmeticCircuitProof {
+        let mut rng = rand::thread_rng();
+        let mut L_vec = Vec::with_capacity(5);
+
+        ArithmeticCircuitProof{
             L_vec: L_vec,
-            R_vec: R_vec,
-            a: a_vec[0],
-            b: b_vec[0],
         }
     }
 }
