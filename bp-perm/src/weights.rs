f69c8155debf1eb28f2cca49e4e52d36f1d4cc2a
ercembu <ercembulut@gmail.com>
2023-09-27 11:54:00 +0200

    added constant_vector test

--- bp-perm/src/weights.rs
+++ bp-perm/src/weights.rs
@@ -8,7 +8,7 @@ use alloc::vec::Vec;
 use core::iter;
 use curve25519_dalek_ng::scalar::Scalar;
 
-fn create_constants(Q: usize) -> Vec<Scalar> {
+pub fn create_constants(Q: usize) -> Vec<Scalar> {
 
     let mut constant_vector: Vec<Scalar> = Vec::new();
     for n in 0..Q-2 {
@@ -17,4 +17,30 @@ fn create_constants(Q: usize) -> Vec<Scalar> {
     constant_vector.push(-Scalar::one());
     constant_vector.push(Scalar::one());
     constant_vector
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_constants(k: usize) {
+        //Deck size k, convert to Q(linear constraint count) from n(multiplication gate count)
+        let Q = k * 2 * 2;
+
+        let c = create_constants(Q);
+        for i in 0..Q-2 {
+            println!("{:#?}",c[i]);
+            assert_eq!(Scalar::zero(), c[i]);
+        }
+        println!("{:#?}",c[Q-2]);
+        assert_eq!(-Scalar::one(), c[Q-2]);
+        println!("{:#?}",c[Q-1]);
+        assert_eq!(Scalar::one(), c[Q-1]);
+    }
+
+    #[test]
+    fn test_1() {
+        test_constants(4);
+    }
+
 }
